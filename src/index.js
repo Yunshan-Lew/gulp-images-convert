305cc6405c675718b799e61afb1a4996cbc62239
刘芸杉 <alexanderde1591@gmail.com>
2017-09-04 14:39:05 +0800

    simply
    simply

--- src/index.js
+++ src/index.js
@@ -6,33 +6,25 @@ var PluginError = gutil.PluginError;
 var PLUGIN_NAME = 'gulp-images-convert';
 
 function imagesConvert(option) {
-  if (!option) {
-    throw new PluginError(PLUGIN_NAME, 'Missing option!');
-  }
-  
-  option = Object.assign({ targetType: 'png', quality: 70 }, option)
-  var targetType = option.targetType
-  var quality = option.quality
+	option = Object.assign({ targetType: 'png', quality: 70 }, option);
+	var targetType = option.targetType,
+		quality = option.quality;
 
-  if (!targetType) {
-    throw new PluginError(PLUGIN_NAME, 'Missing targetType')
-  }
+	var stream = through.obj(function (file, enc, cb) {
+		if (file.isStream()) {
+			this.emit('error', new PluginError(PLUGIN_NAME, 'Streams are not supported'));
+			return cb();
+		}
+		if (file.isBuffer()) {
+			file.contents = images(file.contents).encode(targetType, quality);
+		}
 
-  var stream = through.obj(function (file, enc, cb) {
-    if (file.isStream()) {
-      this.emit('error', new PluginError(PLUGIN_NAME, 'Streams are not supported'))
-      return cb();
-    }
-    if (file.isBuffer()) {
-      file.contents = images(file.contents).encode(targetType, { operation: quality })
-    }
+		this.push(file);
 
-    this.push(file);
+		cb();
+	})
 
-    cb()
-  })
-
-  return stream
+	return stream;
 }
 
 module.exports = imagesConvert;
